37d79d556f6428d90616f860842064d6bafd294f
Haven Selph <Havenselph@gmail.com>
2023-03-17 19:10:29 -0500

    Added random.cb to builtins

--- src/cobralang/interpreter/builtins/__init__.py
+++ src/cobralang/interpreter/builtins/__init__.py
@@ -1,4 +1,5 @@
 all_builtins = {
     "math":"./src/cobralang/interpreter/builtins/math.cb",
     "utils":"./src/cobralang/interpreter/builtins/utils.cb",
+    "random":"./src/cobralang/interpreter/builtins/random.cb",
 }
